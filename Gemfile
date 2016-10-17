3954fb178e82ca351f8642038d2fe6a1d6005d0d
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-10-17 17:50:35 +0300

    Puma attempt #2

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ group :development do
   gem 'capistrano-rails'
   gem 'capistrano-bundler'
   gem 'capistrano-rbenv'
-  gem 'capistrano3-puma',  '~> 1.2.1'
+  gem 'capistrano3-puma', github: "seuros/capistrano-puma"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :log_level, :info
 # set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/settings.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
