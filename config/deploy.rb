75877873c4eb883a847226a8e7183cf48f154f84
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-10-18 17:10:12 +0300

    Remove Bin from linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ set :log_level, :info
 # set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
