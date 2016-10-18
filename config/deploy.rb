8e314032c5140ad1d91e96aa579db4c0e64649c8
Oleg Nikitashin <olegnikitashin@gmail.com>
2016-10-18 16:45:28 +0300

    Return to previous commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
-# config valid only for Capistrano 3.1
+# config valid only for current version of Capistrano
 lock '3.6.1'
+
 set :application, 'spa_backend'
 set :repo_url, 'git@github.com:olegnikitashin/spa_backend.git'
 
@@ -8,14 +9,9 @@ set :branch, 'master'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/spa_backend'
+set :deploy_to, '/home/deploy/applications/spa_backend'
 
 set :log_level, :info
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
 
 # Default value for :scm is :git
@@ -42,19 +38,13 @@ set :log_level, :info
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :linked_files, %w{config/database.yml config/settings.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
 
 set :puma_init_active_record, true
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
-end
